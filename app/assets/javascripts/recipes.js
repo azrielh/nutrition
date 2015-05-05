9b12b3eebb06873938279913c0f3f174e47f144c
azrielh <azhopkins@gmail.com>
2015-05-04 19:18:37 -0700

    added js function for querying and added form fields for the nutritional information

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
 
   $(document).on("keyup", ".search-term", function(){
-    console.log($(this).parents(".nested-fields").find(".protein-field").val());
-    $(this).parents(".nested-fields").find(".protein-field").val($(this).val());
+    var value = this.value;
+    var $form = $(this).parents('.ingredient');
+    var callback = function (data) {
+      //$form
+      //$form.find('.calories-field')
+      //$form.find('.calories-field').val(data[0].nf_calories)
+
+      //debugger
+    };
+    search(value, callback);
+    // console.log($(this).parents(".nested-fields").find(".protein-field").val());
+    // $(this).parents(".nested-fields").find(".protein-field").val($(this).val());
   });
 
   var r_query = {
       "appId":"2aeb108e",
       "appKey":"ecadde5cddbbc58d754489cf310ac8fb",
-      "fields":"item_name,brand_name,nf_calories,nf_serving_size_qty,nf_serving_weight_grams,nf_serving_size_unit,nf_protein,nf_vitamin_a_dv",
+      "fields":"item_name,brand_name,item_id,nf_calories,nf_total_fat,nf_saturated_fat,nf_trans_fatty_acid,nf_cholesterol,nf_sodium,nf_total_carbohydrate,nf_dietary_fiber,nf_sugars,nf_protein,nf_vitamin_a_dv,nf_vitamin_c_nv,nf_calcium_dv,nf_iron_dv,nf_serving_weight_grams,nf_serving_size_qty,nf_serving_size_unit",
       "sort":{
         "field":"_score",
         "order":"desc"
@@ -18,36 +28,29 @@ $(document).ready(function() {
       }
     };
 
-  var query = "chicken salad";
-
-  $.ajax({
-    url:
-    'https://api.nutritionix.com/v1_1/search/' + encodeURIComponent(query),
-    //'https://api.nutritionix.com/v1_1/search/chicken?results=0:30&fields=item_name,brand_name,item_id,nf_calories,nf_protein,nf_total_fat,nf_serving_weight_grams&appId=d2c25d42&appKey=2658c4be2e37caf1223143041f118274',
-    type: 'GET',
-    data: r_query,
-    //beforeSend: function(xhr) { xhr.setRequestHeader('appId','2aeb108e'); xhr.setRequestHeader('appKey', 'ecadde5cddbbc58d754489cf310ac8fb');
-    //},
-    error: function (data) {
-    console.log(r_query);
-    //alert('error');
-  },
-    success: function (data) {
-      console.log(data.hits);
-      //console.log(data.hits[2].fields.item_name);
-      var template = $('#food-test-template').html();
-      //console.log(template);
-      for(var i = 0; i < data.hits.length; i++) {
-        //console.log(ingredient);
-        var ingredient = data.hits[i].fields;
-        var html = Mustache.render(template, ingredient);
-        $('.food').append(html);
+  function search (input, callback) {
+    $.ajax({
+      url:
+      'https://api.nutritionix.com/v1_1/search/' + encodeURIComponent(input),
+      type: 'GET',
+      data: r_query,
+      error: function (data) {
+      alert('error');
+      console.log(r_query);
+      },
+      success: function (data) {
+        var result = data.hits.map(function (datum) { return datum.fields });
+        callback(result);
+        console.log(data.hits);
+        console.log(result);
+        var template = $('#food-test-template').html();
+        for(var i = 0; i < data.hits.length; i++) {
+          var ingredient = data.hits[i].fields;
+          var html = Mustache.render(template, ingredient);
+          $('.food').append(html);
+        }
       }
-    }
-  });
-
+    });
+  }
 
 });
-
-
-//https://api.nutritionix.com/v1_1/search/mcdonalds?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=APPID&appKey=APPKEY
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,7 @@
 class Category < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
-
   has_many :recipes
-
-  #has_many :categorizations, dependent: :destroy
-  #has_many :recipes, through: :categorizations
-
 
   def self.category_search(cat)
     where(name: cat).first.recipes
