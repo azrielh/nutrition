5bf10896fdf3e2ed754092e990ce55c2f92cb8e0
azrielh <azhopkins@gmail.com>
2015-05-13 15:22:53 -0700

    added styling and aws link

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,5 +3,37 @@ class Ingredient < ActiveRecord::Base
 
   validates :qty, presence: true, numericality: { greater_than: 0 }
   validates :name, presence: true
+  validates :calories, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :total_fat, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :saturated_fat, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :cholesterol, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :sodium, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :total_carbs, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :dietary_fiber, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :sugars, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :protein, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :vitamin_a, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :vitamin_c, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :calcium, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :iron, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  after_create :set_zeros
+
+  def set_zeros
+    self.calories ||= 0
+    self.total_fat ||= 0
+    self.saturated_fat ||= 0
+    self.trans_fat ||= 0
+    self.cholesterol ||= 0
+    self.sodium ||= 0
+    self.total_carbs ||= 0
+    self.dietary_fiber ||= 0
+    self.sugars ||= 0
+    self.protein ||= 0
+    self.vitamin_a ||= 0
+    self.vitamin_c ||= 0
+    self.calcium ||= 0
+    self.iron ||= 0
+  end
 
 end
