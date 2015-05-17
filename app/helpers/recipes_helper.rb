025ce2fc62aa47f185f431097b3a7586a980eecb
azrielh <azhopkins@gmail.com>
2015-05-16 18:04:26 -0700

    refactor the nutritional facts table

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -90,4 +90,99 @@ module RecipesHelper
     end
   end
 
+  def weight_per_serving
+    (@recipe.total_weight / @recipe.servings).round(1)
+  end
+
+  def calories_per_serving
+    (@recipe.calories / @recipe.servings).round(0)
+  end
+
+  def calories_from_fat_per_serving
+    calorie_multiplier = 9
+    ((@recipe.total_fat * calorie_multiplier)/@recipe.servings).round(0)
+  end
+
+  def total_fat_per_serving
+    (@recipe.total_fat/@recipe.servings).round(0)
+  end
+
+  def dv_total_fat_per_serving
+    max_total_fat = 65
+    ((@recipe.total_fat / max_total_fat * 100)/@recipe.servings).round(0)
+  end
+
+  def sat_fat_per_serving
+    (@recipe.saturated_fat/@recipe.servings).round(0)
+  end
+
+  def dv_sat_fat_per_serving
+    max_sat_fat = 20
+    ((@recipe.saturated_fat / max_sat_fat * 100)/@recipe.servings).round(0)
+  end
+
+  def trans_fat_per_serving
+    (@recipe.trans_fat/@recipe.servings).round(0)
+  end
+
+  def cholesterol_per_serving
+    (@recipe.cholesterol/@recipe.servings).round(0)
+  end
+
+  def dv_cholesterol_per_serving
+    max_cholesterol = 300
+    ((@recipe.cholesterol / max_cholesterol * 100)/@recipe.servings).round(0)
+  end
+
+  def sodium_per_serving
+    (@recipe.sodium/@recipe.servings).round(0)
+  end
+
+  def dv_sodium_per_serving
+    max_sodium = 2400
+    ((@recipe.sodium / max_sodium * 100)/@recipe.servings).round(0)
+  end
+
+  def carbs_per_serving
+    (@recipe.total_carbs/@recipe.servings).round(0)
+  end
+
+  def dv_carbs_per_serving
+    max_carbs = 300
+    ((@recipe.total_carbs / max_carbs * 100)/@recipe.servings).round(0)
+  end
+
+  def dietary_fiber_per_serving
+    (@recipe.dietary_fiber/@recipe.servings).round(0)
+  end
+
+  def dv_dietary_fiber_per_serving
+    max_dietary_fiber = 25
+    (@recipe.dietary_fiber / max_dietary_fiber * 100).round(0)
+  end
+
+  def sugar_per_serving
+    (@recipe.sugars/@recipe.servings).round(0)
+  end
+
+  def protein_per_serving
+    (@recipe.protein/@recipe.servings).round(0)
+  end
+
+  def dv_vitamin_a_per_serving
+    (@recipe.vitamin_a/@recipe.servings).round(0)
+  end
+
+  def dv_vitamin_c_per_serving
+    (@recipe.vitamin_c/@recipe.servings).round(0)
+  end
+
+  def dv_calcium_per_serving
+    (@recipe.calcium/@recipe.servings).round(0)
+  end
+
+  def dv_iron_per_serving
+    (@recipe.iron/@recipe.servings).round(0)
+  end
+
 end
